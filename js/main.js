0c52e7b5777ad8bd46ddaea69aa52cb92e1102b0
Kostya-bilyznyi <konstantin.biliznyy@gmail.com>
2020-09-24 20:13:17 +0300

    hot sliders and header fix

--- js/main.js
+++ js/main.js
@@ -1,46 +1,38 @@
-'use strict';
+"use strict";
+
+
+const header = document.querySelector(".ba-general-header");
+const sectionOne = document.querySelector(".ba-simple");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("dark-backgrould");
+    } else {
+      header.classList.remove("dark-backgrould");
+    }
+  });
+},
+sectionOneOptions);
+	
+sectionOneObserver.observe(sectionOne);
+
 const bestSlider = $('.ba-sliders');
 bestSlider.slick({
+	dots: true,
 	infinite: true,
-	slidesToShow: 3,
-	slidesToScroll: 1,
-	nextArrow: '.ba-sliders__next',
-	prevArrow: '.ba-sliders__prev',
-	responsive: [
-		{
-			breakpoint: 1024,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 1,
-
-			}
-		},
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 1
-			}
-		},
-		{
-			breakpoint: 480,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-		  }
-		}
-	]
+	speed: 500,
+	fade: true,
+	appendDots: '.ba-slick-dots',
+	autoplay: true,
+	autoplaySpeed: 5000,
+	arrows : false,
+	cssEase: 'linear'
 });
-
-// Tabs
-
-
-const links = $('.ba-farcry-tabs__links');
-
-	links.on('click', function () {
-
-		$('.ba-farcry-tabs__links.active').removeClass('active');
-		$(this).addClass('active');
-		$(this).closest('div.ba-farcry-tabs').find('div.ba-farcry-tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-
-	})
